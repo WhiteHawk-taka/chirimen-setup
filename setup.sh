13bcf9046c186329205a78f0e2fa60a7fac9cd43
WhiteHawk-taka <white-hawk-taka@outlook.com>
2018-08-26 14:44:20 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -27,6 +27,7 @@ echo 'hdmi_force_hotplug=1 hdmi_group=2 hdmi_mode=85 hdmi_drive=2' | sudo tee -a
 
 read -p "enter: "
 # 日本語設定
+
 sudo sed 's/#\sen_GB\.UTF-8\sUTF-8/en_GB\.UTF-8 UTF-8/g' /etc/locale.gen | sudo tee /etc/locale.gen
 sudo sed 's/#\sja_JP\.EUC-JP\sEUC-JP/ja_JP\.EUC-JP EUC-JP/g' /etc/locale.gen | sudo tee /etc/locale.gen
 sudo sed 's/#\sja_JP\.UTF-8\sUTF-8/ja_JP\.UTF-8 UTF-8/g' /etc/locale.gen | sudo tee /etc/locale.gen
@@ -54,7 +55,8 @@ sudo n 8.10.0
 read -p "enter: "
 # カメラを有効化
 sudo raspi-config nonint do_camera 0
-echo 'options bcm2835-v4l2 gst_v4l2src_is_broken=1' | sudo tee -a /etc/modprobe.d/bcm2835-v4l2.conf echo 'bcm2835-v4l2' | sudo tee -a /etc/modules-load.d/modules.conf
+echo 'options bcm2835-v4l2 gst_v4l2src_is_broken=1' | sudo tee -a /etc/modprobe.d/bcm2835-v4l2.conf
+echo 'bcm2835-v4l2' | sudo tee -a /etc/modules-load.d/modules.conf
 
 read -p "enter: "
 # I2Cを有効化
@@ -77,6 +79,7 @@ mv /home/pi/chirimen-setup/Bookmarks /home/pi/.config/chromium/Default/Bookmarks
 
 read -p "enter: "
 # gc設定
+
 sudo sed 's/\/var\/www\/html/\/home\/pi\/Desktop\/gc/g' /etc/apache2/sites-available/000-default.conf | sudo tee /etc/apache2/sites-available/000-default.conf
 sudo sed 's/\/var\/www\//\/home\/pi\/Desktop\/gc/g' /etc/apache2/apache2.conf | sudo tee /etc/apache2/apache2.conf
 sudo cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/vhost-ssl.conf
