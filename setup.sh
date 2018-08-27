73c28a7231648d986a771615e6e4a256700eae92
WhiteHawk-taka <white-hawk-taka@outlook.com>
2018-08-27 23:12:42 +0900

    add cert for chromium

--- setup.sh
+++ setup.sh
@@ -72,12 +72,15 @@ ln -s /home/pi/_gc/srv/reset.sh /home/pi/Desktop/reset.sh
 mkdir /home/pi/.config/chromium/
 mkdir /home/pi/.config/chromium/Default/
 mv /home/pi/chirimen-setup/Bookmarks /home/pi/.config/chromium/Default/Bookmarks
+pcmanfm --set-wallpaper /home/pi/_gc/wallpaper/wallpaper-720p.png
+
 
 # gc設定
 cd /home/pi/
 wget https://rawgit.com/chirimen-oh/chirimen-raspi3/master/release/env/gc.zip
 unzip ./gc.zip -d /home/pi/Desktop
 sudo sed 's/\/var\/www\/html/\/home\/pi\/Desktop\/gc/g' /etc/apache2/sites-available/000-default.conf  | sudo tee /tmp/apache-default && sudo cat /tmp/apache-default | sudo tee /etc/apache2/sites-available/000-default.conf && rm -f /tmp/apache-default
+sudo sed 's/\/var\/www\/html/home\/pi\/Desktop\/gc/g' /etc/apache2/sites-available/vhost-ssl.conf  | sudo tee /tmp/vhost && sudo cat /tmp/vhost | sudo tee /etc/apache2/sites-available/vhost-ssl.conf && rm -f /tmp/vhost
 sudo sed 's/\/var\/www\//\/home\/pi\/Desktop\/gc/g' /etc/apache2/apache2.conf | sudo tee /tmp/apache && sudo cat /tmp/apache | sudo tee /etc/apache2/apache2.conf && rm -f /tmp/apache
 sudo cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/vhost-ssl.conf
 sudo sed 's/\/etc\/ssl\/certs\/ssl-cert-snakeoil\.pem/\/home\/pi\/_gc\/srv\/crt\/server\.crt/g' /etc/apache2/sites-available/vhost-ssl.conf | sudo tee /tmp/vhost && sudo cat /tmp/vhost | sudo tee /etc/apache2/sites-available/vhost-ssl.conf && rm -f /tmp/vhost
@@ -86,6 +89,31 @@ sudo a2ensite vhost-ssl
 sudo a2enmod ssl
 sudo systemctl restart apache2
 echo '@/usr/bin/chromium-browser https://localhost/top' >> /home/pi/.config/lxsession/LXDE-pi/autostart
+
+# 証明書追加
+certfile="/home/pi/_gc/srv/crt/ca.crt"
+certname="org-TripArts"
+
+###
+### For cert8 (legacy - DBM)
+###
+
+for certDB in $(find ~/ -name "cert8.db")
+do
+    certdir=$(dirname ${certDB});
+    certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d dbm:${certdir}
+done
+
+###
+### For cert9 (SQL)
+###
+
+for certDB in $(find ~/ -name "cert9.db")
+do
+    certdir=$(dirname ${certDB});
+    certutil -A -n "${certname}" -t "TCu,Cu,Tu" -i ${certfile} -d sql:${certdir}
+done
+
 
 # Arduino IDE 追加
 cd /home/pi/
@@ -99,3 +127,5 @@ cd /home/pi/Applications/arduino/
 ./install.sh
 rm -f /home/pi/arduino-1.8.6-linuxarm.tar.xz
 cd /home/pi/
+
+reboot
